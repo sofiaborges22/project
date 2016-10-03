aa0b4faaabeafe898b3bb5f09e41981f777c5ae1
Sofia Borges <borgessofia22@gmail.com>
2016-10-03 09:50:35 +0200

    Password reset works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+  config.action_controller.default_url_options = { host: 'finalproject.com' }
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
